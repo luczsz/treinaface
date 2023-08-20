a9e1a7fefd41470e826598e8ce5b2c27e2c33ed1
Lucas <francisco.lucs@gmail.com>
2023-08-20 20:14:35 -0300

    HOME - Buttons

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,11 @@
-  import React from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
 
 import { styles } from './style';
 import Header from '../../components/Header';
+import { theme } from '../../global/theme';
 
 export default function Home() {
  return (
@@ -10,7 +13,38 @@ export default function Home() {
         <Header/>
         <View style={styles.content} >
 
-        <Text>HME</Text>
+        <Text style={{fontSize: 22, fontWeight: '500', color: theme.colors.secondary, marginBottom: 10,}}>
+        Escolha uma categoria abaixo e vamos começar ?
+        </Text>
+
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={[styles.button, {backgroundColor: theme.colors.primary }]}
+          >
+            <Text style={styles.title} >
+              básico
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={[styles.button, {backgroundColor: theme.colors.secondary }]}
+          >
+            <Text style={styles.title}>
+              Médio
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={[styles.button, {backgroundColor: theme.colors.tercery }]}
+          >
+            <Text style={styles.title}>
+              Difícil
+            </Text>
+          </TouchableOpacity>
+
+
         </View>
    </View>
   );
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,9 +1,25 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '../../global/theme';
 
 export const styles = StyleSheet.create({
     content:{
         height: '100%',
         backgroundColor: 'white',
 
+        padding:20,
+    },
+    button:{
+        height: 120,
+        marginBottom: 10,
+        borderRadius: 12,
+
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+    },
+    title:{
+        fontSize: 70,
+        fontWeight: 'bold',
+        color: theme.colors.white090,
+        textTransform: 'uppercase'
     }
 })
