447aadf500aa83acda52b354310edab8f6408124
Lucas <francisco.lucs@gmail.com>
2023-08-24 01:27:17 -0300

    PROGRESS - designer

--- src/pages/Progress/index.js
+++ src/pages/Progress/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { styles } from './style';
+import { MaterialCommunityIcons, Entypo } from '@expo/vector-icons';
+import { theme } from '../../global/theme';
+
 export default function Progress() {
  return (
    <View style={styles.container} >
-        <Text style={{ fontSize: 22, fontWeight: 'bold', color: 'white' }} >
-            Olá Usuario, pagina em desenvolvimento.
-        </Text>
+        <View style={styles.header} >
+            <MaterialCommunityIcons name='progress-star' size={34} color={theme.colors.secondary} />
+
+            <Text style={{ fontSize: 22, fontWeight: 'bold', color: theme.colors.secondary }} >
+                Seu progresso
+            </Text>
+        </View>
+        <View style={styles.content} >
+            <View style={styles.progresed}>
+                <Entypo name='progress-one' size={34} color={theme.colors.secondary} />
+                <Text style={styles.title} >
+                    nivel 1
+                </Text>
+                <Text style={styles.subTitle} >
+                    - Em progresso
+                </Text>
+            </View>
+        </View>
    </View>
   );
 }
-
-export const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: 'green',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-})
---
+++ src/pages/Progress/style.js
@@ -0,0 +1,52 @@
+import { StyleSheet } from 'react-native';
+
+import { theme } from '../../global/theme';
+
+export const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: 'white',
+    },
+    header:{
+        flexDirection: 'row',
+        marginTop: 31,
+        height: 90,
+        alignItems: 'center',
+        gap: 8,
+
+        paddingLeft: 14,
+        paddingRight: 14,
+
+        borderBottomWidth: 2,
+        borderBottomColor: theme.colors.secondary,
+    },
+
+    content:{
+        
+        flex: 1,
+
+        padding: 20,
+    },
+    progresed:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 7,
+
+        padding: 14,
+        backgroundColor: theme.colors.white090,
+        borderRadius: 12,
+    },
+
+    title:{
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: theme.colors.secondary,
+        textTransform: 'uppercase',
+    },
+    subTitle:{
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: theme.colors.highlight,
+        
+    },
+})
